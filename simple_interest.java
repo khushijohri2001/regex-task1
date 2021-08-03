f997ed2ef63bd2d267407a3fd0fc933d6d932bb1
khushijohri2001 <khushijohri2001@gmail.com>
2021-08-03 10:48:26 +0530

    feat: first commit

---
+++ simple_interest.java
@@ -0,0 +1,32 @@
+//Q.1: Write a program to calculate simple interest (make sure to take principle amount, interest rate and time period from console)
+
+
+
+import java.util.Scanner;
+
+public class simple_interest {
+
+	
+	public static void main(String[] args) {
+		
+		@SuppressWarnings("resource")
+		Scanner sc=new Scanner(System.in);
+				
+		System.out.println("Enter Principle Amount: ");
+		float principle= sc.nextFloat();
+		
+		System.out.println("Enter Interest Rate: ");
+		float rate= sc.nextFloat();
+		
+		System.out.println("Enter Time Period: ");
+		float time= sc.nextFloat();
+		
+		float si=principle*rate*time;
+		
+		System.out.println("The Simple Interest is: Rs. " + si);
+		
+		
+
+	}
+
+}
